df3a497b12d279f2f3777b1b37427ba38b5d0968
eddipietro <94018665+eddipietro@users.noreply.github.com>
2024-03-22 10:33:01 -0300

    whatsAppButton

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,27 @@ import Cart from "./components/Cart/Cart";
 import Venta from "./components/Venta/Venta";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import CartCustomProvider from "./context/CartContext";
+import WhatsAppButton from "./components/whatsApp/whatsApp";
 
 function App() {
   return (
     <HashRouter>
       <CartCustomProvider>
         <NavBar />
-           <Routes>
+        <Routes>
           <Route
             path="/"
-            element={<ItemListContainer mensaje=" Productos" />}
+            element={<ItemListContainer mensaje="Productos" />}
           />
           <Route path="/category/:categoryId" element={<ItemListContainer />} />
           <Route path="/detail/:id" element={<ItemDetailContainer />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/venta" element={<Venta />} />
         </Routes>
+        <WhatsAppButton /> {/* Coloca el botón de WhatsApp en todas las páginas */}
       </CartCustomProvider>
     </HashRouter>
-  
   );
 }
+
 export default App;
